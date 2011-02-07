7533b241a5fd2500db15abf4d29c1c3d12535194
TestSubject06 <zzzaaccckk@yahoo.com>
2011-02-06 21:47:28 -0500

    wtf

--- Serverside Code/Serverside Code/Game Code/Game.cs
+++ Serverside Code/Serverside Code/Game Code/Game.cs
@@ -5,22 +5,27 @@ using System.Collections;
 using PlayerIO.GameLibrary;
 using System.Drawing;
 
-namespace DrawPad {
-	//Player class. each player that join the game will have these attributes.
-	public class Player : BasePlayer {
+namespace DrawPad
+{
+    //Player class. each player that join the game will have these attributes.
+    public class Player : BasePlayer
+    {
         public int playerIndex;
         public int playerX = 200;
         public int PlayerY = 200;
-	}
+    }
 
     [RoomType("Crawler")]
     public class GameCode2 : Game<Player>
     {
         private int numPlayers = 0;
+        private int levelSeed = 10;
         public override void GameStarted()
         {
             // anything you write to the Console will show up in the 
             // output window of the development server
+            //Random rnd = new Random();
+            //levelSeed = rnd.Next(0, 60000);
             Console.WriteLine("Crawler game has started");
         }
 
@@ -47,12 +52,14 @@ namespace DrawPad {
             int PI = 0;
             int numInstances = 0;
             Boolean temp = false;
-            do{
+            do
+            {
                 temp = false;
                 numInstances = 0;
                 foreach (Player p in Players)
                 {
-                    if(p.playerIndex == PI){
+                    if (p.playerIndex == PI)
+                    {
                         numInstances++;
                     }
                 }
@@ -62,11 +69,11 @@ namespace DrawPad {
                     PI++;
                     temp = true;
                 }
-            }while(temp == true);
+            } while (temp == true);
             player.playerIndex = PI;
             //Send info about all already connected users to the newly joined users chat
             Message m = Message.Create("GameInit");
-            m.Add(player.playerIndex);
+            m.Add(player.playerIndex, levelSeed);
 
             foreach (Player p in Players)
             {
@@ -119,63 +126,75 @@ namespace DrawPad {
         }
     }
 
-	[RoomType("DrawPad")]
-	public class GameCode : Game<Player> {
-		// This method is called when an instance of your the game is created
-		public override void GameStarted() {
-			// anything you write to the Console will show up in the 
-			// output window of the development server
-			Console.WriteLine("Game is started");
-		}
+    [RoomType("DrawPad")]
+    public class GameCode : Game<Player>
+    {
+        // This method is called when an instance of your the game is created
+        public override void GameStarted()
+        {
+            // anything you write to the Console will show up in the 
+            // output window of the development server
+            Console.WriteLine("Game is started");
+        }
 
-		// This method is called when the last player leaves the room, and it's closed down.
-		public override void GameClosed() {
-			Console.WriteLine("RoomId: " + RoomId);
-		}
+        // This method is called when the last player leaves the room, and it's closed down.
+        public override void GameClosed()
+        {
+            Console.WriteLine("RoomId: " + RoomId);
+        }
 
-		// This method is called whenever a player joins the game
-		public override void UserJoined(Player player) {
-			
-			//Send info about all already connected users to the newly joined users chat
-			Message m = Message.Create("ChatInit");
-			m.Add(player.Id);
+        // This method is called whenever a player joins the game
+        public override void UserJoined(Player player)
+        {
 
-			foreach(Player p in Players) {
-				m.Add(p.Id, p.ConnectUserId);
-			}
+            //Send info about all already connected users to the newly joined users chat
+            Message m = Message.Create("ChatInit");
+            m.Add(player.Id);
 
-			player.Send(m);
+            foreach (Player p in Players)
+            {
+                m.Add(p.Id, p.ConnectUserId);
+            }
 
-			//Informs other users chats that a new user just joined.
-			Broadcast("ChatJoin", player.Id, player.ConnectUserId);
-		}
+            player.Send(m);
 
-		// This method is called when a player leaves the game
-		public override void UserLeft(Player player) {
-			//Tell the chat that the player left.
-			Broadcast("ChatLeft", player.Id);
-		}
+            //Informs other users chats that a new user just joined.
+            Broadcast("ChatJoin", player.Id, player.ConnectUserId);
+        }
 
-		// This method is called when a player sends a message into the server code
-		public override void GotMessage(Player player, Message message) {
-			switch(message.Type) {
-				case "start": {
-						Broadcast("start", player.Id, player.ConnectUserId, message.GetInt(0), message.GetInt(1));
-						break;
-					}
-				case "stop": {
-						Broadcast("stop", player.Id);
-						break;
-					}
-				case "move": {
-						Broadcast("move", player.Id, message.GetInt(0), message.GetInt(1));
-						break;
-					}
-				case "ChatMessage":{
-						Broadcast("ChatMessage", player.Id, message.GetString(0));
-						break;
-					}
-			}
-		}
-	}
+        // This method is called when a player leaves the game
+        public override void UserLeft(Player player)
+        {
+            //Tell the chat that the player left.
+            Broadcast("ChatLeft", player.Id);
+        }
+
+        // This method is called when a player sends a message into the server code
+        public override void GotMessage(Player player, Message message)
+        {
+            switch (message.Type)
+            {
+                case "start":
+                    {
+                        Broadcast("start", player.Id, player.ConnectUserId, message.GetInt(0), message.GetInt(1));
+                        break;
+                    }
+                case "stop":
+                    {
+                        Broadcast("stop", player.Id);
+                        break;
+                    }
+                case "move":
+                    {
+                        Broadcast("move", player.Id, message.GetInt(0), message.GetInt(1));
+                        break;
+                    }
+                case "ChatMessage":
+                    {
+                        Broadcast("ChatMessage", player.Id, message.GetString(0));
+                        break;
+                    }
+            }
+        }
+    }
 }
